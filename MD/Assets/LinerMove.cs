b57590767b57204272249c1c78fcd81efdf2e796
yaster <fr87s.9875@gmail.com>
2016-11-25 11:32:49 +0900

    add project files

---
+++ MD/Assets/LinerMove.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LinerMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(RePositionWithDelay());
+	}
+	
+    IEnumerator RePositionWithDelay() {
+		while (true) {
+			SetPosition();
+			yield return new WaitForSeconds(5);
+		}
+    }
+
+	void SetPosition() {
+		float x = Random.Range(-5.0f, 5.0f);
+		float z = Random.Range(-5.0f, 5.0f);
+		Debug.Log("X,Z: " + x.ToString("F2") + "," + z.ToString("F2"));
+		transform.position = new Vector3(x, 0.0f, z);
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ MD/Assets/moveCar.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveCar : MonoBehaviour {
+
+	private Vector3 m_pos;
+	// Use this for initialization
+	void Start() {
+		m_pos = transform.localPosition;  // 形状位置を保持
+	}
+
+	// Update is called once per frame
+	void Update() {
+		transform.localPosition = m_pos;  // 形状位置を更新
+		m_pos.x += -0.3f;
+	}
+}
